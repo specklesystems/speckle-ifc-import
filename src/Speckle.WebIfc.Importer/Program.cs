2ae8a792efea7ec7c2fe5b4e9cc0f01edfa31bda
Adam Hathcock <adam@hathcock.uk>
2024-11-15 12:02:11 +0000

    add importer

---
+++ src/Speckle.WebIfc.Importer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection; 
+using Microsoft.Extensions.DependencyInjection; 
+using Speckle.Objects.Geometry; 
+using Speckle.Sdk.Host; 
+using Speckle.Sdk.Models;
+using Speckle.WebIfc.Importer;
+using Speckle.WebIfc.Importer.Converters;
+using Speckle.WebIfc.Importer.Ifc;
+
+TypeLoader.Initialize(typeof(Base).Assembly, typeof(Point).Assembly); 
+var serviceCollection = new ServiceCollection(); 
+serviceCollection.AddSpeckleWebIfc(); 
+serviceCollection.AddMatchingInterfacesAsTransient(Assembly.GetExecutingAssembly()); 
+ 
+var serviceProvider = serviceCollection.BuildServiceProvider(); 
+ 
+var factory = serviceProvider.GetRequiredService<IIfcFactory>(); 
+Console.WriteLine(factory.Version); 
+ 
+var model = factory.Open("C:\\Users\\adam\\Git\\engine_web-ifc\\examples\\example.ifc"); 
+ 
+var converter = serviceProvider.GetRequiredService<IModelConverter>(); 
+var b = converter.Convert(model); 
+Console.WriteLine(b.GetTotalChildrenCount());
