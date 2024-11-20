0a378bffb998751261329d90ac3ea2a106f15045
Adam Hathcock <adam@hathcock.uk>
2024-11-20 08:31:54 +0000

    add import default

--- src/Speckle.WebIfc.Importer/Import.cs
+++ src/Speckle.WebIfc.Importer/Import.cs
@@ -1,39 +1,89 @@
+using System.Diagnostics;
 using System.Reflection;
 using Ara3D.IfcParser;
 using Ara3D.Utils;
 using Microsoft.Extensions.DependencyInjection;
 using Speckle.Objects.Geometry;
+using Speckle.Sdk;
 using Speckle.Sdk.Api;
+using Speckle.Sdk.Api.GraphQL.Inputs;
+using Speckle.Sdk.Api.GraphQL.Models;
+using Speckle.Sdk.Common;
+using Speckle.Sdk.Credentials;
 using Speckle.Sdk.Host;
 using Speckle.Sdk.Models;
+using Speckle.Sdk.Serialisation.V2;
+using Speckle.Sdk.Serialisation.V2.Send;
+using Speckle.Sdk.Transports;
 using Speckle.WebIfc.Importer.Converters;
 using Speckle.WebIfc.Importer.Ifc;
 
 namespace Speckle.WebIfc.Importer;
 public static class Import
 {
-  
-  private static readonly Uri BaseUri = new Uri("https://api.github.com/");
+  public static async Task Ifc(string url, string filePath, string streamId,
+    string modelId, string commitMessage, string token, IProgress<ProgressArgs>? progress = null)
+  {
+    var serviceProvider = GetServiceProvider();
+    await Ifc(serviceProvider, url, filePath,  streamId, modelId, commitMessage, token, progress);
+  }
 
-  public static async Task Ifc(string filePath, string userId, string streamId, string branchName, string commitMessage, string token)
+  public static ServiceProvider GetServiceProvider()
   {
     TypeLoader.Initialize(typeof(Base).Assembly, typeof(Point).Assembly);
     var serviceCollection = new ServiceCollection();
+    serviceCollection.AddSpeckleSdk(HostApplications.Other, HostAppVersion.v2024, "IFC-Importer");
     serviceCollection.AddSpeckleWebIfc();
     serviceCollection.AddMatchingInterfacesAsTransient(Assembly.GetExecutingAssembly());
+    return serviceCollection.BuildServiceProvider();
+  }
 
-    var serviceProvider = serviceCollection.BuildServiceProvider();
-    var operations = serviceProvider.GetRequiredService<IOperations>();
+  public static async Task Ifc(IServiceProvider serviceProvider, string url, string filePath, string streamId,
+    string modelId, string commitMessage, string token, IProgress<ProgressArgs>? progress = null)
 
-    var factory = serviceProvider.GetRequiredService<IIfcFactory>();
-    Console.WriteLine(factory.Version);
+  {
+    var ifcFactory = serviceProvider.GetRequiredService<IIfcFactory>();
+    var clientFactory = serviceProvider.GetRequiredService<IClientFactory>();
+    var baseUri = new Uri(url);
+    Console.WriteLine(ifcFactory.Version);
+    var stopwatch = Stopwatch.StartNew();
 
-    var model = factory.Open(filePath);
+    var model = ifcFactory.Open(filePath);
+    var ms = stopwatch.ElapsedMilliseconds;
+    Console.WriteLine($"Opened with WebIFC: {ms} ms");
 
     var graph = IfcGraph.Load(new FilePath(filePath));
+    var ms2 = stopwatch.ElapsedMilliseconds;
+    Console.WriteLine($"Loaded with StepParser: {ms2 - ms} ms");
 
     var converter = serviceProvider.GetRequiredService<IGraphConverter>();
     var b = converter.Convert(model, graph);
-    var (rootId, _) = await operations.Send2(BaseUri, streamId, token, b);
+    ms = ms2;
+    ms2 = stopwatch.ElapsedMilliseconds;
+    Console.WriteLine($"Converted to Speckle Bases: {ms2 - ms} ms");
+
+    var serializeProcessFactory = serviceProvider.GetRequiredService<ISerializeProcessFactory>();
+    var process = serializeProcessFactory.CreateSerializeProcess(baseUri, streamId, token, progress);
+    var (rootId, _) = await process.Serialize(b, default, new SerializeProcessOptions(true, true, false))
+      .ConfigureAwait(false);
+    Account account = new() { token = token, serverInfo = new ServerInfo { url = baseUri.ToString(), } };
+    ms = ms2;
+    ms2 = stopwatch.ElapsedMilliseconds;
+    Console.WriteLine($"Uploaded to Speckle: {ms2 - ms} ms");
+
+    // 8 - Create the version (commit)
+    using var apiClient = clientFactory.Create(account);
+    _ = await apiClient
+      .Version.Create(
+        new CreateVersionInput(
+          rootId,
+          modelId,
+          streamId,
+          message: commitMessage
+        )
+      );
+    ms = ms2;
+    ms2 = stopwatch.ElapsedMilliseconds;
+    Console.WriteLine($"Committed to Speckle: {ms2 - ms} ms");
   }
 }
